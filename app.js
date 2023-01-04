cc3783944dcf588fe325f24f5819bba9a9c702e6
shawnelldh <54282973+shawnelldh@users.noreply.github.com>
2023-01-04 00:57:00 -0500

    1 file modified

---
+++ app.js
@@ -0,0 +1,23 @@
+if ( favsItems.length ===0 )  {
+  favsSection.style.display = 'none';
+};
+
+
+
+} else (tag ==='path') {
+  if (baseline === 'delete' || basevalue === 'cap'  || basevalue === 'bin') {
+   let li = event.target.parentNode.parentNode.parentNode;
+   let ul = li.parentNode;
+   ul.removeChild
+
+
+
+
+
+  }
+
+
+
+
+
+}
